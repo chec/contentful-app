8df11ef13eaf73f9da250cdc5aa369742d12ab5a
Guy Marriott <guy@chec.io>
2021-12-15 15:04:29 -0800

    Allow removal of products with an action menu option

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -74,6 +74,21 @@ const Field = (props: FieldProps) => {
     return fieldSdk.setValue(isMultiSelect ? result : result[0]);
   }
 
+  const handleDeleteProduct = (id: string) => {
+    const index = fieldValue.findIndex(candidate => candidate === id);
+    if (index < 0) {
+      return;
+    }
+
+    fieldSdk.setValue(isMultiSelect
+      ? [
+        ...fieldValue.slice(0, index),
+        ...fieldValue.slice(index + 1),
+      ]
+      : id,
+    );
+  }
+
   const commerceSdk = useMemo(() => {
     return new Commerce(publicKey, false);
   }, [publicKey]);
@@ -81,6 +96,13 @@ const Field = (props: FieldProps) => {
   useEffect(() => {
     if (fieldValue.length === 0 || !commerceSdk) {
       setProducts([]);
+      return;
+    }
+
+    // Skip loading if we already have all the relevant products
+    if (fieldValue.every(id => products.find(candidate => candidate.id === id))) {
+      // Just filter out products that aren't in the field value
+      setProducts(products.filter(candidate => fieldValue.includes(candidate.id)));
       return;
     }
 
@@ -114,6 +136,9 @@ const Field = (props: FieldProps) => {
         }}
       >
         Open in Chec Dashboard
+      </DropdownListItem>
+      <DropdownListItem onClick={() => handleDeleteProduct(id)}>
+        Delete
       </DropdownListItem>
     </DropdownList>
   )
